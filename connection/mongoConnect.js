b760b3ac020b7411096cea03538b0e4f82da7690
JOSE FELIPE ESCOBAR <89820647+JoseFelipeEscobar@users.noreply.github.com>
2022-06-12 19:19:02 -0500

    Update mongoConnect.js

--- connection/mongoConnect.js
+++ connection/mongoConnect.js
@@ -5,11 +5,12 @@ const host=process.env.MONGO_HOST;
 const port=process.env.MONGO_PORT;
 const db=process.env.DB_NAME;
 
+
 /**
  * Function to connect the database
  *  */ 
 exports.mongoConnect = () => {
-const mongoStringConnection = `mongodb://${host}:${port}/${db}`;
+const mongoStringConnection = "mongodb+srv://admin:admin@auth.h3bqd.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(mongoStringConnection,{useNewUrlParser:true, useUnifiedTopology:true})
     .then(()=>console.log("success, mongo db connected"))
     .catch(()=>console.log("error en base de datos: "+ err));
